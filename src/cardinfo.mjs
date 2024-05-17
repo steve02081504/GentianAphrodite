ab98f113e9236758aa068da76410de193bf0db46
steve02081504 <steve02081504@EDEN.fukwold>
2024-05-17 10:26:52 +0800

    soft lock of build

--- src/cardinfo.mjs
+++ src/cardinfo.mjs
@@ -53,7 +53,7 @@ function yamlWriting(yamlObj) {
  */
 function arraysEqual(a, b) {
 	if (a === b) return true;
-	if (a == null || b == null) return false;
+	if (!a || !b) return false;
 	if (a.length !== b.length) return false;
 
 	for (var i = 0; i < a.length; ++i)
--- src/path/build.mjs
+++ src/path/build.mjs
@@ -2,7 +2,7 @@ import CardFileInfo from "../cardinfo.mjs";
 
 CardFileInfo.readDataFiles();
 if ((process.argv[2] ??= 'default') == 'default')
-	console.log(`building Card to ${process.argv[3] || `./build/${process.argv[2]}.png`}`);
+	console.log(`building card to ${process.argv[3] || `./build/${process.argv[2]}.png`}`);
 else
 	console.log(`building subVer: ${process.argv[2]} to ${process.argv[3] || `./build/${process.argv[2]}.png`}`);
-CardFileInfo.Build(process.argv[2], process.argv[3]);
+if (process.env.EdenOS) CardFileInfo.Build(process.argv[2], process.argv[3]);
