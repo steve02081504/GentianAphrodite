377345ec7d14e6921b5d61e22556d0259a3701ba
steve02081504 <steve02081504@EDEN.fukwold>
2024-05-29 10:15:56 +0800

    Update stat.mjs

--- .esh/commands/stat.mjs
+++ .esh/commands/stat.mjs
@@ -4,17 +4,20 @@ import { get_encoding } from "tiktoken";
 
 var encoder = get_encoding('o200k_base');
 function simple_marco_remover(str) {
-	return str.replace(/{{\/\/([\s\S]*?)}}/g, '').
-	replace(/\{\{user\}\}/gi, 'user').
-	replace(/\{\{char\}\}/gi, 'char');
+	return str.replace(/{{\/\/([\s\S]*?)}}/g, '').replace(/\{\{(user|char)\}\}/gi, 'name');
 }
-CardFileInfo.readDataFiles();
-keyScoreAdder(CardFileInfo.character_book.entries)
+CardFileInfo.readDataFiles(); keyScoreAdder(CardFileInfo.character_book.entries)
 let charData = CardFileInfo.metaData, enabledWIs = CardFileInfo.character_book.entries.filter(_ => _.enabled)
 let stat = {
 	data_size: (JSON.stringify(CardFileInfo.v1metaData).length / 1024.0).toFixed(2) + 'KB',
-	enabled_wibook_entries: enabledWIs.length,
-	disabled_wibook_entries: CardFileInfo.character_book.entries.length - enabledWIs.length,
+	wibook_entries: {
+		total: CardFileInfo.character_book.entries.length,
+		enabled: {
+			total: enabledWIs.length,
+			bluelight: enabledWIs.filter(_ => _.constant).length,
+		},
+		disabled: CardFileInfo.character_book.entries.length - enabledWIs.length,
+	},
 	key_num: enabledWIs.map(_ => _.keys.length + _.secondary_keys.length).reduce((a, b) => a + b, 0),
 	total_token_size: encoder.encode(simple_marco_remover([
 		charData.description, charData.personality, charData.scenario, charData.mes_example,
@@ -24,4 +27,4 @@ let stat = {
 	].join('\n')).replace(/\n+/g, '\n')).length
 }
 encoder.free();
-console.dir(stat,{ depth: null });
+console.dir(stat, { depth: null });
