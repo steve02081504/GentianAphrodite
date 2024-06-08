8cfc005b73bef3c3cb5ce6c8965c6cf3bb83804a
steve02081504 <steve02081504@EDEN.fukwold>
2024-06-08 10:38:58 +0800

    add keyScore before test

--- .esh/commands/test.mjs
+++ .esh/commands/test.mjs
@@ -1,8 +1,10 @@
 import { CardFileInfo } from "../../src/cardinfo.mjs";
 import { encoder_free } from "../../src/get_token_size.mjs";
+import { keyScoreAdder } from "../../src/keyScore.mjs";
 import { promptBuilder } from "../../src/prompt_builder.mjs";
 
 CardFileInfo.readDataFiles();
+keyScoreAdder(CardFileInfo.character_book.entries);
 let result = promptBuilder(CardFileInfo.metaData, process.argv[2] || 'Have a nice pee.');
 encoder_free()
 for (let key in result) if (!result[key]?.length) delete result[key];
