73a05c0efc04c4d1f4df79ce46a0d0bfc547c260
steve02081504 <steve02081504@EDEN.fukwold>
2024-09-04 15:25:18 +0800

    add more token size stat

--- .esh/commands/stat.mjs
+++ .esh/commands/stat.mjs
@@ -2,9 +2,11 @@ import CardFileInfo from "../../src/cardinfo.mjs"
 import { keyScoreAdder } from "../../src/keyScore.mjs"
 import { remove_simple_marcos } from "../../src/tools.mjs"
 import { encoder, get_token_size } from "../../src/get_token_size.mjs"
+import { promptBuilder } from "../../src/prompt_builder.mjs"
 
 CardFileInfo.readDataFiles(); keyScoreAdder(CardFileInfo.character_book.entries)
 let charData = CardFileInfo.metaData, enabledWIs = CardFileInfo.character_book.entries.filter(_ => _.enabled)
+let greetings = [charData.first_mes, ...charData.alternate_greetings, ...charData.extensions.group_greetings]
 let stat = {
 	data_size: (JSON.stringify(CardFileInfo.v1metaData).length / 1024.0).toFixed(2) + 'KB',
 	wibook_entries: {
@@ -19,9 +21,20 @@ let stat = {
 	total_token_size: get_token_size(remove_simple_marcos([
 		charData.description, charData.personality, charData.scenario, charData.mes_example,
 		charData.system_prompt, charData?.extensions?.depth_prompt?.prompt,
-		charData.first_mes, ...charData.alternate_greetings, ...charData.extensions.group_greetings,
+		...greetings,
 		...enabledWIs.map(_ => _.content),
-	].filter(_ => _).join('\n')).replace(/\n+/g, '\n'))
+	].filter(_ => _).join('\n')).replace(/\n+/g, '\n')),
+	base_token_size: get_token_size(promptBuilder(charData, 'hello')),
+	normal_token_size: greetings.map(greeting => get_token_size(promptBuilder(charData, [
+		{
+			role: 'assistant',
+			content: greeting
+		},
+		{
+			role: 'user',
+			content: 'hello'
+		}
+	]))).reduce((a, b) => a + b, 0)/greetings.length,
 }
 encoder.free()
 console.dir(stat, { depth: null })
