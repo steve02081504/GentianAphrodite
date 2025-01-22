a09822f5084e6d50d13702b9542396e1907de953
steve02081504 <steve02081504@EDEN.fukwold>
2025-01-22 19:27:47 +0800

    workaround of https://github.com/denoland/deno/issues/27768

--- reply_gener/functions/coderunner.mjs
+++ reply_gener/functions/coderunner.mjs
@@ -4,14 +4,9 @@ import { parse } from 'npm:acorn'
 import { walk } from 'npm:estree-walker'
 import { generate } from 'npm:astring'
 import { builders } from 'npm:ast-types'
-import { bash_exec, pwsh_exec } from '../../scripts/exec.mjs'
+import { bash_exec_NATS, pwsh_exec_NATS } from '../../scripts/exec.mjs'
 /** @typedef {import("../../../../../../../src/public/shells/chat/decl/chatLog.ts").chatLogEntry_t} chatLogEntry_t */
 /** @typedef {import("../../../../../../../src/decl/prompt_struct.ts").prompt_struct_t} prompt_struct_t */
-
-function removeTerminalSequences(str) {
-	// deno-lint-ignore no-control-regex
-	return str.replace(/\x1B\[[\d;]*[Km]/g, '')
-}
 
 /**
  * @param {chatLogEntry_t} result
@@ -87,12 +82,12 @@ export async function coderunner(result, { addLongTimeLog }) {
 			console.info('AI运行的Powershell代码：', pwshrunner)
 			pwshrunner = `&{\n${pwshrunner}\n} | Out-String -Width 65536`
 			let pwshresult
-			try { pwshresult = await pwsh_exec(pwshrunner) } catch (err) { pwshresult = err }
+			try { pwshresult = await pwsh_exec_NATS(pwshrunner) } catch (err) { pwshresult = err }
 			console.info('pwshresult', pwshresult)
 			addLongTimeLog({
 				name: 'system',
 				role: 'system',
-				content: '执行结果：\nstdout：\n' + removeTerminalSequences(pwshresult.stdout) + '\nstderr：\n' + removeTerminalSequences(pwshresult.stderr),
+				content: '执行结果：\n' + util.inspect(pwshresult),
 				files: []
 			})
 			result.extension.execed_codes[pwshrunner] = pwshresult
@@ -110,12 +105,12 @@ export async function coderunner(result, { addLongTimeLog }) {
 			})
 			console.info('AI运行的Bash代码：', bashrunner)
 			let bashresult
-			try { bashresult = await bash_exec(bashrunner) } catch (err) { bashresult = err }
+			try { bashresult = await bash_exec_NATS(bashrunner) } catch (err) { bashresult = err }
 			console.info('bashresult', bashresult)
 			addLongTimeLog({
 				name: 'system',
 				role: 'system',
-				content: '执行结果：\nstdout：\n' + removeTerminalSequences(bashresult.stdout) + '\nstderr：\n' + removeTerminalSequences(bashresult.stderr),
+				content: '执行结果：\n' + util.inspect(bashresult),
 				files: []
 			})
 			result.extension.execed_codes[bashrunner] = bashresult
--- scripts/exec.mjs
+++ scripts/exec.mjs
@@ -1,26 +1,65 @@
 import { exec as base_exec } from 'node:child_process'
 import { promisify } from 'node:util'
-const exec = promisify(base_exec)
+export const exec = promisify(base_exec)
 
-// if pwsh.exe available, use it, else use powershell.exe
-let pwshavailable = Boolean(await exec('1', { 'shell': 'pwsh.exe' }).catch(() => false))
-
-export function pwsh_exec(code) {
-	return exec(code, { 'shell': pwshavailable ? 'pwsh.exe' : 'powershell.exe' })
-}
-export function powershell_exec(code) {
-	return exec(code, { 'shell': 'powershell.exe' })
-}
 export function bash_exec(code) {
 	return exec(code, { 'shell': '/bin/bash' })
 }
-export {
-	exec
+
+async function testShellPaths(paths) {
+	for (const path of paths)
+		if(await exec('1', { 'shell': path }).catch(() => false))
+			return path
 }
 
+let powershellPath = await testShellPaths([
+	'powershell.exe',
+	'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe',
+])
+let pwshPath
+export function powershell_exec(code) {
+	return exec(code, { 'shell': powershellPath })
+}
+export function pwsh_exec(code) {
+	return exec(code, { 'shell': pwshPath ?? powershellPath })
+}
 export function where_command(command) {
 	if (process.platform === 'win32')
 		return pwsh_exec(`Get-Command -Name ${command} -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Definition`).then(result => result.stdout.trim())
 	else
 		return bash_exec(`which ${command}`).then(result => result.stdout.trim())
+}
+pwshPath = await testShellPaths([
+	'pwsh.exe',
+	await where_command('pwsh'),
+])
+
+export function removeTerminalSequences(str) {
+	// deno-lint-ignore no-control-regex
+	return str.replace(/\x1B\[[\d;]*[Km]/g, '')
+}
+function RemoveterminalSequencesFromExecResult(result) {
+	return {
+		...result,
+		stdout: removeTerminalSequences(result.stdout),
+		stderr: removeTerminalSequences(result.stderr)
+	}
+}
+
+// no ansi terminal sequences functions
+
+export function exec_NATS(str) {
+	return exec(str).then(RemoveterminalSequencesFromExecResult)
+}
+
+export function bash_exec_NATS(str) {
+	return bash_exec(str).then(RemoveterminalSequencesFromExecResult)
+}
+
+export function pwsh_exec_NATS(str) {
+	return pwsh_exec(str).then(RemoveterminalSequencesFromExecResult)
+}
+
+export function powershell_exec_NATS(str) {
+	return powershell_exec(str).then(RemoveterminalSequencesFromExecResult)
 }
