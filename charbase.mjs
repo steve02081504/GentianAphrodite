fc1fde507198e60f0b7c8ec8c14d24412eac7301
steve02081504 <steve02081504@EDEN.fukwold>
2025-02-19 14:11:49 +0800

    fix of charvar getting

--- charbase.mjs
+++ charbase.mjs
@@ -3,7 +3,11 @@ import { exec } from './scripts/exec.mjs'
 
 export const chardir = import.meta.dirname
 export const charurl = `/chars/${encodeURIComponent(path.basename(chardir))}`
-export const charvar = await exec('git describe --tags', { cwd: chardir }).then((result) => result.stdout.trim())
+export const charvar = await exec('git describe --tags', { cwd: chardir }).then((result) => result.stdout.trim()).catch(
+	() => exec('git rev-parse --short HEAD', { cwd: chardir }).then((result) => result.stdout.trim()).catch(
+		() => 'unknown'
+	)
+)
 export let username = ''
 
 export function initCharBase(init) {
