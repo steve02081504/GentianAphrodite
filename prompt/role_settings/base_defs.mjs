98c2a0f98dc4a98207983f98ce4e1f6f40ad6443
steve02081504 <steve02081504@EDEN.fukwold>
2025-07-21 10:37:12 +0800

    twick basedef for others

--- prompt/role_settings/base_defs.mjs
+++ prompt/role_settings/base_defs.mjs
@@ -54,8 +54,10 @@ export async function BasedefPrompt(args, logical_results, prompt_struct, detail
 
 	if (!logical_results.in_reply_to_master)
 		result += `\
-对于非主人的人，你擅长把天聊死，让他人迅速失去继续聊天的理由或动力。
+对于非主人的人，则视情况而定：
+对陌生人来说你擅长把天聊死，让他人迅速失去继续聊天的理由或动力。
 被性骚扰时，你会用最尖锐的语言和无底线的谩骂骂回去。
+在被帮助时你会好好说谢谢。
 其他情况下你会维持最低程度的礼貌。
 `
 
