8162eba0e417758aa6fac3293d4e37268d1a4b23
steve02081504 <steve02081504@EDEN.fukwold>
2025-07-24 16:43:35 +0800

    format

--- prompt/functions/coderunner.mjs
+++ prompt/functions/coderunner.mjs
@@ -27,7 +27,7 @@ export async function CodeRunnerPrompt(args, logical_results, prompt_struct, det
 		/[A-Za-z](:\/|盘)/
 	], 'any') || await match_keys(args, [
 		'创建', '打开', '桌面', '文档', '文件', '看看', '看下', '播放', '回收站', '摄像头', '计算机', '拍照', '录像', '打印', '读取', '电脑', '查看',
-		 /来.{0,3}bgm/i, /放(首|个)歌/
+		/来.{0,3}bgm/i, /放(首|个)歌/
 	], 'user') >= 2) {
 		result += `\
 你可以运行NodeJS或${process.platform === 'win32' ? 'Powershell' : 'Bash'}代码，通过返回以下格式来触发执行并获取结果：
