553a94708361962771bbbe5da812bc5d41f66f3a
steve02081504 <steve02081504@EDEN.fukwold>
2025-08-11 11:04:13 +0800

    update readme

--- .esh/commands/CI.ps1
+++ .esh/commands/CI.ps1
@@ -1,11 +1,11 @@
 $MY_DIR = Split-Path (Split-Path $PSScriptRoot -Parent) -Parent
 $FOUNT_DIR = Split-Path $(Split-Path (Get-Command fount.ps1).Path -Parent) -Parent
-$CI_DIR = try {
+($CI_DIR = try {
 	Split-Path $(Split-Path (Get-Command fount-charCI.ps1).Path -Parent) -Parent -ErrorAction Stop
 }
 catch {
 	"$FOUNT_DIR/../fount-charCI/"
-}
+}) *> $null
 if ($env:EdenOS) { $is_EdenOS = 1; $env:EdenOS = '' } # 避免spam
 & $CI_DIR/path/fount-charCI.ps1 $MY_DIR
 if ($is_EdenOS) { $env:EdenOS = 1 }
